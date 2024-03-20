33133d578c88bb8447351effade3b674327e458e
Kartik Agarwal <kartikhustles@gmail.com>
2024-03-20 20:16:22 +0530

    Macbook Scroll Effect Created

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MacbookScrollDemo } from "./components/main-elements/MacbookScrollDemo";
+
+const page = () => {
+  return (
+    <div>
+      <MacbookScrollDemo />
+    </div>
+  );
+};
+
+export default page;
